99640bf84f462b3a9d9e176e5ebf13540d4ebfcc
Matheus <matheuscanovas9@gmail.com>
2024-06-25 17:33:39 -0300

    feat: creating the view structure PaymentPage

---
+++ Fender.App/Views/PaymentPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Fender.App.Views;
+
+public partial class PaymentPage : Page
+{
+    public PaymentPage()
+    {
+        InitializeComponent();
+    }
+}
