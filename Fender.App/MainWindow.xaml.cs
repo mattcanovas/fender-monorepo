c84a8a56af3f314b755f501965fe633a0a4b96d5
Matheus <matheuscanovas9@gmail.com>
2024-06-25 16:07:32 -0300

    feat: creating the view HomePage;

--- Fender.App/MainWindow.xaml.cs
+++ Fender.App/MainWindow.xaml.cs
@@ -8,6 +8,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        _mainFrame.Navigate(new AuthPage());
+        _mainFrame.Navigate(new HomePage());
     }
 }
---
+++ Fender.App/Views/HomePage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Fender.App.Views;
+
+public partial class HomePage : Page
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
