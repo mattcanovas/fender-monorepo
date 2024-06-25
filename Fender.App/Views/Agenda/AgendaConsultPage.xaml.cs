38828b763ea3838fe52f5878a7e85461ca4beb30
Matheus <matheuscanovas9@gmail.com>
2024-06-25 17:44:30 -0300

    feat: implementing the view of AgendaConsultPage

---
+++ Fender.App/Views/Agenda/AgendaConsultPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Fender.App.Views.Agenda;
+
+public partial class AgendaConsultPage : Page
+{
+    public AgendaConsultPage()
+    {
+        InitializeComponent();
+    }
+}
